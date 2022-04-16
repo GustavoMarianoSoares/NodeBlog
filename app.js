7008ae67c15b804ed0ec29bf7c73f2adeaa12585
Mariano <gustavomarianosoares@gmail.com>
2022-04-16 15:48:49 -0300

    Listagem de categorias

--- app.js
+++ app.js
@@ -10,6 +10,8 @@
     const flash = require('connect-flash')
     require('./models/Postagem')
     const Postagem = mongoose.model('postagens')
+    require('./models/Categoria')
+    const Categoria = mongoose.model('categorias')
 
 //Configuracoes
   //Sessão
@@ -64,6 +66,37 @@
         res.redirect('/')
       })
     })
+
+    app.get('/categorias',(req, res) => {
+      Categoria.find().lean().then((categorias) =>{
+        res.render('categorias/index', {categorias: categorias})
+      }).catch(err => {
+        console.log(err)
+        req.flash('error_msg','Houve um erro')
+        res.redirect('/')
+      })
+    })
+
+    app.get('/categorias/:slug', (req, res) => {
+      Categoria.findOne({slug: req.params.slug}).lean().then((categoria)=>{
+        if(categoria){
+          Postagem.find({categoria: categoria._id}).lean().then((postagens)=>{
+            res.render('categorias/postagens',{postagens: postagens, categoria: categoria})
+          }).catch((err)=>{
+            req.flash('error_msg', 'Houve um erro')
+            res.redirect('/')
+          })
+        }else{
+          req.flash('error_msg', 'Esta categoria não existe')
+          res.redirect('/')
+        }
+      }).catch(err => {
+        req.flash('error_msg', 'Houve um erro')
+        res.redirect('/')
+      })
+    })
+
+
 
     app.get('/404',(req, res) => {
       res.send('Erro 404')
